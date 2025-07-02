8411cce29b08ac7e30754be2d48fbf3f660a951c
GANJIK <109108826+GANJIK@users.noreply.github.com>
2025-07-02 19:22:36 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-_01",
+      "name": "тур_01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 875,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "АКЦЕНТ 3D вид комната",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
